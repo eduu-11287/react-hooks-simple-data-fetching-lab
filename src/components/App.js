091aa5f32feff529ade641bdaf11d4081e20656f
Edwin <edmboche141@gmail.com>
2025-01-24 08:32:31 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,33 @@
 // create your App component here
+import React, {useState, useEffect } from 'react';
+function App() {
+    const [dogImage, setDogImage] =  useState(null);
+    const[isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then((response) => response.json())
+        .then((data) => {
+            setDogImage(data.message);
+            setIsLoading(false);
+        })
+            .catch((error) => {
+                console.log('Error fetiching dog imsge:', error);
+                setIsLoading(false);
+            });
+        }, []);
+    
+        return (
+            <div>
+                {isLoading ?(
+                    <p>Loading...</p>
+                ) : dogImage ?(
+                    <img src={dogImage} alt="random dog image"/>
+                ) : (
+                    <p>Failed to load.</p>
+                )}
+            </div>
+        );
+}_
+
+export default App;
